61865693733de24004cd8a5cb917d8207374e185
zxw <1012596161@qq.com>
2019-05-23 13:18:23 +0800

    框架

---
+++ src/components/adressError.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div style="margin-top: 15px;">
+      <el-input placeholder="请输入地址" v-model="input4" clearable>
+        <template slot="append">
+          <a href="#" @click="adrAnaly">地址纠错查询</a>
+        </template>
+      </el-input>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      input4: ""
+    };
+  },
+  components: {},
+  methods: {
+    adrAnaly() {}
+  }
+};
+</script>
+<style scoped>
+a {
+  list-style: none;
+  text-decoration: none;
+  color: #909399;
+}
+</style>
+
---
+++ src/components/headerNav.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="地址分析" name="first">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6">
+          <div class="grid-content bg-purple">
+            <adressAnalysis></adressAnalysis>
+          </div>
+        </el-col>
+      </el-row>
+    </el-tab-pane>
+    <el-tab-pane label="地址别名查询" name="second">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6">
+          <div class="grid-content bg-purple">
+            <adressSelect></adressSelect>
+          </div>
+        </el-col>
+      </el-row>
+    </el-tab-pane>
+    <el-tab-pane label="地址纠错" name="third">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6">
+          <div class="grid-content bg-purple">
+            <adressError></adressError>
+          </div>
+        </el-col>
+      </el-row>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+import adressAnalysis from "./adressAnalysis.vue";
+import adressSelect from "./adressSelect.vue";
+import adressError from "./adressError.vue";
+export default {
+  data() {
+    return {
+      activeName: "first"
+    };
+  },
+  components: {
+    adressAnalysis,
+    adressSelect,
+    adressError
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
