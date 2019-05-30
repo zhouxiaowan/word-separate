bd2c311b0502c170f39989a568611508ad59602b
zxw <1012596161@qq.com>
2019-05-30 16:27:20 +0800

    fixed

--- jiekou.js
+++ jiekou.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const app = express();
 app.use('/', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "http://172.21.39.76:8080"); //项目上线后改成页面的地址
+    res.header("Access-Control-Allow-Origin", "http://10.9.74.145:8080"); //项目上线后改成页面的地址
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.send({
         best_match_result: {
             "match_source": 0,
+            "paichusuo": "小官庄派出所",
             "score": 70.70113,
             "source": {
                 "city": "扬州市",
                 "community": "贾桥社区",
-                "county": "邗江区",
+                "county": "邗江区邗江区",
                 "hamlet": "",
                 "location": [
                     119.387927,
                     32.385495
                 ],
-                "paichusuo": "邗上派出所",
-                "road": "望月路",
-                "village": "邗上街道"
+                "paichusuo": "小官庄派出所",
+                "road": "观南路",
+                "village": "小官庄镇"
             }
         },
         rest_match_result: [{
                 "match_source": 0,
+                "paichusuo": "邗上派出所",
                 "score": 68.60188,
                 "source": {
                     "city": "扬州市",
@@ -41,77 +43,72 @@ app.use('/', function (req, res) {
             },
             {
                 "match_source": 0,
+                "paichusuo": "邗上派出所",
                 "score": 68.60188,
                 "source": {
                     "city": "扬州市",
                     "community": "文昌社区",
-                    "county": "邗江区",
+                    "county": "邗江区文昌社区",
                     "hamlet": "",
                     "location": [
                         119.387927,
                         32.385495
                     ],
                     "paichusuo": "",
-                    "road": "望月路",
-                    "village": "邗上街道"
-                }
-            }
-        ],
-
-        dataList: [{
-                best_match_source: 0,
-                score: 70.70113,
-                source: {
-                    city: "扬州市",
-                    community: "贾桥社区",
-                    county: "邗江区",
-                    hamlet: "",
-                    location: [119.387927, 32.385495],
-                    paichusuo: "邗上派出所",
-                    road: "望月路",
-                    village: "邗上街道"
+                    "road": "望月路文昌社区",
+                    "village": "邗上街道文昌社区"
                 }
             },
             {
-                best_match_source: 0,
-                score: 68.60188,
-                source: {
-                    city: "扬州市",
-                    community: "文昌社区",
-                    county: "邗江区",
-                    hamlet: "",
-                    location: [119.387927, 32.385495],
-                    paichusuo: "邗上派出所",
-                    road: "望月路aaaaaaaa",
-                    village: "邗上街道"
+                "match_source": 0,
+                "paichusuo": "邗上派出所",
+                "score": 68.60188,
+                "source": {
+                    "city": "扬州市",
+                    "community": "文昌社区",
+                    "county": "邗江区文昌社区",
+                    "hamlet": "",
+                    "location": [
+                        119.387927,
+                        32.385495
+                    ],
+                    "paichusuo": "",
+                    "road": "望月路文昌社区",
+                    "village": "邗上街道文昌社区"
                 }
             },
             {
-                best_match_source: 0,
-                score: 68.60188,
-                source: {
-                    city: "扬州市",
-                    community: "文昌社区",
-                    county: "邗江区",
-                    hamlet: "",
-                    location: [119.387927, 32.385495],
-                    paichusuo: "hahahahahah",
-                    road: "望月路",
-                    village: "邗上街道"
+                "match_source": 0,
+                "paichusuo": "邗上派出所",
+                "score": 68.60188,
+                "source": {
+                    "city": "扬州市",
+                    "community": "文昌社区",
+                    "county": "邗江区文昌社区",
+                    "hamlet": "",
+                    "location": [
+                        119.387927,
+                        32.385495
+                    ],
+                    "paichusuo": "",
+                    "road": "望月路文昌社区",
+                    "village": "邗上街道文昌社区"
                 }
             }
         ],
         result: [{
-                "end": 2,
-                "start": 0,
-                "word": "望月路"
-            },
-            {
-                "end": 6,
-                "start": 3,
-                "word": "全季酒店"
-            }
-        ]
+            "tag": 0,
+            "word": "\u5168\u5b63\u9152\u5e97"
+        }, {
+            "tag": 1,
+            "word": "\u65c1\u8fb9\u7684"
+        }, {
+            "tag": 0,
+            "word": "\u7684\u9f99\u867e\u5e97"
+        }, {
+            "tag": 1,
+            "word": "\u65c1\u8fb9\u7684\u9f99\u867e\u5e97"
+        }]
     });
 });
 app.listen(3000, function () {
--- src/components/adressAnalysis.vue
+++ src/components/adressAnalysis.vue
@@ -3,40 +3,55 @@
     <el-row>
       <el-col :span="18" :offset="3">
         <div style="margin-top: 15px;">
-          <el-input v-model="address" clearable>
+          <el-input v-if="address" v-model="address" @keyup.enter.native="adrAnaly" clearable>
             <template slot="append">
               <a href="#" @click="adrAnaly">地址分析</a>
             </template>
           </el-input>
+          <el-input v-else v-model="address" clearable>
+            <template slot="append">
+              <a href="#">地址分析</a>
+            </template>
+          </el-input>
           <div class="tags">
-            <el-tag v-for="(tag,index) in tags" :key="index" closable :disable-transitions="false" @close="handleClose(tag)">{{tag}}</el-tag>
+            <el-row>
+              <el-col :span="21" style="text-align:left">
+                <el-tag :type="tagtypes[index]===0?'':'info'" v-for="(tag,index) in tags" :key="index" closable :disable-transitions="false" @close="handleClose(tag)">{{tag}}</el-tag>
+              </el-col>
+              <el-col :span="3">
+                <p v-if="tags.length" class="clear_p">
+                  <img @click="handleSplitWord" class="clear_input" src="../assets/img/clear.png">
+                </p>
+              </el-col>
+            </el-row>
           </div>
         </div>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="20" :offset="2">
+      <el-col class="split-content">
         <div class="adressResult" v-if="is_ResultList">
           <p class="split-result">切分结果</p>
           <div class="split-result-list" v-if="best_match_result">
             <el-row>
-              <el-col :span="16">
+              <el-col :span="16" class="split-adress">
                 <div class="Result-list">
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.city}}</el-button>
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.community}}</el-button>
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.county}}</el-button>
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.paichusuo}}</el-button>
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.road}}</el-button>
-                  <el-button round v-if="best_match_result.source">{{best_match_result.source.village}}</el-button>
+                  <el-button round v-if="best_match_result.source.city">{{best_match_result.source.city}}</el-button>
+                  <el-button round v-if="best_match_result.source.community">{{best_match_result.source.community}}</el-button>
+                  <el-button round v-if="best_match_result.source.county">{{best_match_result.source.county}}</el-button>
+                  <el-button round v-if="best_match_result.source.road">{{best_match_result.source.road}}</el-button>
+                  <el-button round v-if="best_match_result.source.village">{{best_match_result.source.village}}</el-button>
                 </div>
               </el-col>
               <el-col :span="7" :offset="1">
                 <p class="tips">
                   <span>相似度:</span>
                   <span v-if="best_match_result.score">{{(best_match_result.score).toFixed(2)}}分</span>
-                  <span style="margin-left:10px">地址来源:</span>
+                  <span style="margin-left:10px">所属地:{{best_match_result.paichusuo}}</span>
+                  <!-- <span style="margin-left:10px">地址来源:</span>
                   <span>{{best_match_result.match_source === 0?'警方地址库':''}}</span>
-                  <span>{{best_match_result.match_source === 1?'Poli库':''}}</span>
+                  <span>{{best_match_result.match_source === 1?'POI库':''}}</span>
+                  <span>{{best_match_result.match_source === 2?'暂无精准匹配来源':''}}</span>-->
                 </p>
               </el-col>
             </el-row>
@@ -48,18 +63,19 @@
                   <el-button round v-if="item.source.city">{{item.source.city}}</el-button>
                   <el-button round v-if="item.source.community">{{item.source.community}}</el-button>
                   <el-button round v-if="item.source.county">{{item.source.county}}</el-button>
-                  <el-button round v-if="item.source.paichusuo">{{item.source.paichusuo}}</el-button>
                   <el-button round v-if="item.source.road">{{item.source.road}}</el-button>
-                  <el-button round v-if="item.source.road">{{item.source.village}}</el-button>
+                  <el-button round v-if="item.source.village">{{item.source.village}}</el-button>
                 </div>
               </el-col>
               <el-col :span="7" :offset="1">
                 <p class="tips">
                   <span>相似度:</span>
                   <span v-if="item.score">{{(item.score).toFixed(2)}}分</span>
-                  <span style="margin-left:10px">地址来源:</span>
-                  <span>{{item.best_match_source === 0?'警方地址库':''}}</span>
-                  <span>{{item.best_match_source === 1?'Poli库':''}}</span>
+                  <span style="margin-left:10px">归属地:{{item.paichusuo}}</span>
+                  <!-- <span style="margin-left:10px">地址来源:</span>
+                  <span>{{item.match_source === 0?'警方地址库':''}}</span>
+                  <span>{{item.match_source === 1?'Poli库':''}}</span>
+                  <span>{{item.match_source === 2?'暂无精准匹配来源':''}}</span>-->
                 </p>
               </el-col>
             </el-row>
@@ -72,10 +88,8 @@
     </el-row>
   </div>
 </template>
-<script>
-import axios from "axios";
-import { type } from "os";
 
+<script>
 export default {
   data() {
     return {
@@ -85,25 +99,51 @@ export default {
       best_match_result: "",
       rest_match_result: "",
       splitWord: "",
-      tags: []
+      tags: [],
+      tagtypes: []
     };
   },
   components: {},
   methods: {
     adrAnaly() {
-      this.$http.get("http://172.21.39.76:3000/").then(response => {
-        this.splitWord = JSON.parse(response.request.response).result;
-        for (var i in this.splitWord) {
-          this.tags.push(this.splitWord[i].word);
-        }
-        this.address = "";
-        this.is_ResultList = true;
+      const loading = this.$loading({
+        lock: true,
+        text: "Loading",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 0.7)"
       });
-      this.$http.get("http://172.21.39.76:3000/").then(response => {
-        this.best_match_result = JSON.parse(response.request.response).best_match_result;
-        this.rest_match_result = JSON.parse(response.request.response).rest_match_result;
-        this.is_ResultList = true;
-      });
+      this.$http
+        .get("http://10.9.74.145:3000/")
+        .then(response => {
+          loading.close();
+          this.splitWord = JSON.parse(response.request.response).result;
+          for (var i in this.splitWord) {
+            this.tags.push(this.splitWord[i].word);
+            this.tagtypes.push(this.splitWord[i].tag);
+          }
+          this.address = "";
+          this.is_ResultList = true;
+        })
+        .catch(err => {
+          loading.close();
+          this.$message.error("哦噢！数据出错了，请联系系统管理员");
+        });
+      this.$http
+        .get("http://10.9.74.145:3000/")
+        .then(response => {
+          loading.close();
+          this.best_match_result = JSON.parse(response.request.response).best_match_result;
+          this.rest_match_result = JSON.parse(response.request.response).rest_match_result;
+          this.is_ResultList = true;
+        })
+        .catch(err => {
+          loading.close();
+          this.$message.error("哦噢！数据出错了，请联系系统管理员");
+        });
+    },
+    handleSplitWord() {
+      this.tags = [];
+      this.is_ResultList = false;
     },
     handleClose(tag) {
       this.tags.splice(this.tags.indexOf(tag), 1);
@@ -120,10 +160,24 @@ a {
   text-decoration: none;
   color: #909399;
 }
+.split-content {
+  min-width: 83.33333%;
+}
+.split-adress {
+  min-width: 60%;
+}
 .del_list {
   width: 40px;
   cursor: pointer;
   margin-top: 50px;
+}
+.clear_p {
+  text-align: right;
+}
+.clear_input {
+  cursor: pointer;
+  width: 20px;
+  vertical-align: -webkit-baseline-middle;
 }
 .tips {
   color: #ffffff;
@@ -134,6 +188,7 @@ a {
   margin-bottom: 40px;
   color: #eee;
   font-size: 18px;
+  text-align: left;
 }
 .split-result-list {
   text-align: left;
@@ -147,6 +202,7 @@ a {
 }
 .tags {
   position: absolute;
+  width: 65%;
   top: 23px;
   left: 13%;
 }
